db2f24b6d8b7f3f208955783d7edb8ea0022781d
debskiszymon <debski.szymon@gmail.com>
2020-12-18 10:38:34 +0100

    deployment

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SelectField
+from wtforms import StringField, TextAreaField, BooleanField, SelectField, PasswordField
 from wtforms.validators import DataRequired
 from blog import db
+from werkzeug.routing import ValidationError
+from config import Config
 
 
 class EntryForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     body = TextAreaField('body', validators=[DataRequired()])
     is_published = BooleanField('is_published')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    def validate_username(self, field):
+        if field.data != Config.ADMIN_USERNAME:
+            raise ValidationError("Invalid username")
+        return field.data
+
+    def validate_password(self, field):
+        if field.data != Config.ADMIN_PASSWORD:
+            raise ValidationError("Invalid password")
+        return field.data
--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,16 @@
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for, session
 from blog import app, db
 from blog.models import Entry
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get('logged_in'):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return check_permissions
 
 @app.route("/")
 def index():
@@ -10,6 +19,7 @@ def index():
 
 @app.route("/new-post/", methods=["GET", "POST"])
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def entry(entry_id=None):
     if entry_id is None:
         form = EntryForm()
@@ -39,4 +49,58 @@ def entry(entry_id=None):
             else:
                 errors = form.errors
         return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get('next')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            session['logged_in'] = True
+            session.permanent = True
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            errors = form.errors
+    return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        flash('You are now logged out.', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/drafts/', methods=['GET', 'POST'])
+@login_required
+def list_drafts():
+    drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+    return render_template("drafts.html", drafts=drafts)
+    
+@app.route("/drafts-entry/<int:entry_id>", methods=["GET", "POST"])
+@login_required
+def delete_entry(entry_id=None):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    form = EntryForm(obj=entry)
+    db.session.delete(entry)
+    db.session.commit()
+    flash(f'Usuniętio wpis {form.title.data}')
+    return redirect(url_for("list_drafts"))
+
+@app.route("/delete-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
+def delete_post(entry_id=None):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    # form = EntryForm(obj=entry)
+    db.session.delete(entry)
+    db.session.commit()
+    # flash(f'Usuniętio wpis {form.title.data}')
+    return redirect(url_for("index"))
+
+
+
+
+
 
